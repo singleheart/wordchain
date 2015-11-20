cc03190d8f7ea1be380ee136b98ce0221f2bcdab
mrthink <mrthinks@gmail.com>
2015-11-20 22:44:22 +0900

    Create myDictionary.py
    add class for dictionary file loading

---
+++ src/game/myDictionary.py
@@ -0,0 +1,52 @@
+'''
+Created on Nov 20, 2015
+
+@author: mrthinks@gmail.com
+'''
+
+# ~/.pyenv/versions/3.4.3/lib/python3.4/site-packages/konlpy/java/data/kE
+#dicPath = '/home/mrthink/.pyenv/versions/3.4.3/lib/python3.4/site-packages/konlpy/java/data/kE/dic_system.txt'
+dicPath = '/usr/local/lib/python3.4/dist-packages/konlpy/java/data/kE/dic_system.txt'
+
+class MyDictionary():
+    dic = None 
+    
+    def loadDic(self):
+        if self.dic is not None:
+            return
+        self.dic = dict()
+            
+        with open(dicPath) as f:
+            lines = f.readlines()
+            
+            for line in lines:
+                word = line.split()[0]
+                start_c = word[0]
+                
+                dic2 = self.dic.get(start_c)
+                if dic2 is None :
+                    dic2 = set()
+                    self.dic[start_c] = dic2
+                dic2.add(word)
+
+        
+    def __init__(self):
+        self.loadDic()
+        return
+    
+    def isExist(self, word):
+        self.loadDic()
+        if len(word) <= 0:
+            return False
+        
+        if word[0] in self.dic:
+            return word in self.dic[word[0]]
+        else:
+            return False
+    
+
+dd = MyDictionary()
+
+if __name__ == '__main__':
+        print(dd.isExist("AIDS"))
+        print(dd.isExist("ADIDAS"))
