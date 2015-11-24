0a9233febd2c982655d29225309ca6cb9fda272a
mrthink <mrthinks@gmail.com>
2015-11-24 14:27:53 +0900

    Update views.py
    test code for error message

--- src/game/views.py
+++ src/game/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.views import generic
+from django.views.generic import DetailView
 from django import forms
 from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
 import datetime
 
 from .myDictionary import MyDictionary, dd
@@ -16,12 +18,24 @@ class IndexView(generic.ListView):
     def get_queryset(self):
         return
 
+class PlayData():
+    def __init__(self, obj, word):
+        self.obj = obj
+        self.word = word
+
 class PlayView(generic.ListView):
     template_name = 'game/play.html'
     context_object_name = 'History'
 
     def get_queryset(self):
-        return History.objects.order_by('-updateDate')[0:10]
+        errWord = self.request.GET.get('errWord')
+        errType = self.request.GET.get('errType')
+        print('request: ', errWord, ", ", errType)
+        
+        if(errWord is None):
+            return {'obj':History.objects.order_by('-updateDate')[0:10]}
+        else:
+            return {'obj':History.objects.order_by('-updateDate')[0:10], 'errWord':errWord, 'errType':errType}
 
 class ScoreView(generic.ListView):
     template_name = 'game/scoreboard.html'
@@ -37,12 +51,13 @@ def get_name(request):
         
         matchedList = History.objects.filter(text=answer)
         if len(matchedList) > 0:
-            print('matchedList: ', matchedList[0].updateDate)
-            return HttpResponseRedirect('play')
-        
+            # print('matchedList: ', matchedList[0].updateDate)
+            return HttpResponseRedirect('play?errWord='+answer+'&errType='+'AlreadyExist')
+
         if not dd.isExist(answer):
-            answer += " (nonword)"
-        
+            # print('NotInDic: ', answer)
+            return HttpResponseRedirect('play?errWord='+answer+'&errType='+'NotInDic')
+
         # db write
         p = History(text=answer, userId=request.user, updateDate= str(datetime.datetime.now()))
         p.save()
