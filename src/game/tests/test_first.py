831f0c7d591d1a097044a0ff6d0a03a7ffa7abe0
singleheart <singleheart@gmail.com>
2016-05-16 03:38:43 +0000

    construct page url from gethostname of socket this method will fail if project number is more than 1

--- src/game/tests/test_first.py
+++ src/game/tests/test_first.py
@@ -1,14 +1,17 @@
 from selenium import webdriver
 from django.test import TestCase
+import socket
 
 class FirstTests(TestCase):
     def test_play_with_no_login(self):
-        page_url = "https://wordchain-lonelywolflee-1.c9users.io/"
+        username, projectname, _ = socket.gethostname().split('-')
+        page_url = "http://" + projectname + "-" + username + ".c9.io/"
+        print(page_url)
 
         driver = webdriver.PhantomJS()
         driver.set_window_size(1120, 550)
         
         driver.get(page_url)
         driver.find_element_by_id("play").click()
-        self.assertEqual("https://wordchain-lonelywolflee-1.c9users.io/accounts/login/?next=/game/", driver.current_url)
+        self.assertEqual(page_url+"accounts/login/?next=/game/", driver.current_url)
         driver.quit()
