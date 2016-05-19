48e41156e8cf0351b655d90a2b370ccf78991f3d
karma822 <karma822@gmail.com>
2016-05-19 03:37:25 +0000

    Fix: play link test with login

--- src/game/tests/test_first.py
+++ src/game/tests/test_first.py
@@ -14,15 +14,23 @@ class FirstTests(TestCase):
         self.assertEqual(page_url+"accounts/login/?next=/game/", driver.current_url)
         driver.quit()
         
-    def test_play_with_login(self):
+    def login(self, driver):
         page_url = "http://" + os.getenv('C9_HOSTNAME') + "/" 
-
-        driver = webdriver.PhantomJS()
-        driver.set_window_size(1120, 550)
         
         driver.get(page_url + "accounts/login/?next=/game/")
         driver.find_element_by_id("id_username").send_keys("test")
         driver.find_element_by_id("id_password").send_keys("test")
         driver.find_element_by_id("login").click()
+        
+    def test_play_with_login(self):
+        driver = webdriver.PhantomJS()
+        driver.set_window_size(1120, 550)
+        
+        self.login(driver)
+        
+        page_url = "http://" + os.getenv('C9_HOSTNAME') + "/"
+        driver.get(page_url)
+        driver.find_element_by_id("play").click()
         self.assertEqual(page_url + "game/", driver.current_url)
+        
         driver.quit()
