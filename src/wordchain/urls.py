4468e80feadc513cdb44ea04f9b8a4051ae466a8
singleheart <singleheart@gmail.com>
2016-06-09 03:40:44 +0000

    add signup

--- src/wordchain/urls.py
+++ src/wordchain/urls.py
@@ -26,4 +26,5 @@ urlpatterns = [
     url(r'^game/', decorator_include(login_required, 'game.urls', namespace='game')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/', include('django.contrib.auth.urls')),
+    url(r'^accounts/register/$', 'wordchain.views.register', name='register'),
 ]
--- src/wordchain/views.py
+++ src/wordchain/views.py
@@ -1,4 +1,8 @@
 from django.views import generic
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
 
 # Create your views here.
 
@@ -7,3 +11,15 @@ class IndexView(generic.ListView):
     
     def get_queryset(self):
         return
+    
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            new_user = form.save()
+            return HttpResponseRedirect("/game")
+    else:
+        form = UserCreationForm()
+    return render(request, "registration/register.html", {
+        'form': form,
+    })
