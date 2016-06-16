5292c451a38773c57bbe505afeae7f4b5bfba1e4
mrthinks <mrthinks@gmail.com>
2016-06-16 03:30:54 +0000

    add logout test

--- src/game/tests/test_login.py
+++ src/game/tests/test_login.py
@@ -4,10 +4,10 @@ import os
 
 class LoginTests(TestCase):
         
-    def login(self, driver, user, password):
+    def login(self, driver, user, password, nextPage=''):
         page_url = "http://" + os.getenv('C9_HOSTNAME') + "/" 
         
-        driver.get(page_url + "accounts/login/")
+        driver.get(page_url + "accounts/login/" + nextPage)
         driver.find_element_by_id("id_username").send_keys(user)
         driver.find_element_by_id("id_password").send_keys(password)
         driver.find_element_by_id("login").click()
@@ -21,4 +21,21 @@ class LoginTests(TestCase):
         driver.get(page_url)
         self.login(driver, "nobody", "None")
         self.assertEqual(page_url, driver.current_url)
+        driver.quit()
+        
+    def test_logout(self):
+        page_url = "http://" + os.getenv('C9_HOSTNAME') + "/game/"
+        accounts_url = "http://" + os.getenv('C9_HOSTNAME') + "/" +"accounts/"
+
+        driver = webdriver.PhantomJS()
+        driver.set_window_size(1120, 550)
+        
+        driver.get(page_url)
+
+        self.login(driver, 'test', 'test', '?next=/game/')
+        self.assertEqual(page_url, driver.current_url)
+        
+        driver.find_element_by_id("logout").click()
+        self.assertRegexpMatches(driver.current_url, accounts_url + 'login' + '.*')
+
         driver.quit()
