1e980ff93fbe6cd1cc8652b25edfd608d4029a95
singleheart <singleheart@gmail.com>
2016-06-23 03:55:50 +0000

    set driver as instance variable

--- src/game/tests/test_signup.py
+++ src/game/tests/test_signup.py
@@ -3,6 +3,10 @@ from django.test import TestCase
 import os
 
 class SignupTests(TestCase):
+    driver = webdriver.PhantomJS()
+    
+    def teardown(self):
+        self.driver.quit()
         
     def login(self, driver, user, password):
         driver.find_element_by_id("id_username").send_keys(user)
@@ -22,17 +26,14 @@ class SignupTests(TestCase):
         page_url = "http://" + os.getenv('C9_HOSTNAME') + "/game/"
         accounts_url = "http://" + os.getenv('C9_HOSTNAME') + "/" +"accounts/"
 
-        driver = webdriver.PhantomJS()
-        driver.set_window_size(1120, 550)
+        self.driver.set_window_size(1120, 550)
         
-        driver.get(page_url)
-        driver.find_element_by_id('signup').click();
-        self.assertEqual(accounts_url + 'register/', driver.current_url)
+        self.driver.get(page_url)
+        self.driver.find_element_by_id('signup').click();
+        self.assertEqual(accounts_url + 'register/', self.driver.current_url)
         
-        self.signup(driver, 'test2', 'test')
-        self.assertRegexpMatches(driver.current_url, accounts_url + 'login' + '.*')
+        self.signup(self.driver, 'test2', 'test')
+        self.assertRegexpMatches(self.driver.current_url, accounts_url + 'login' + '.*')
         
-        self.login(driver, 'test2', 'test')
-        self.assertEqual(page_url, driver.current_url)
-        
-        driver.quit()
+        self.login(self.driver, 'test2', 'test')
+        self.assertEqual(page_url, self.driver.current_url)
